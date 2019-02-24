621625df2cb57f6276f7463dfb6d5c78fd43c8c3
ajlopez <webmaster@ajlopez.com>
2019-02-24 13:36:40 -0300

    Scan character

--- test/scanners.js
+++ test/scanners.js
@@ -14,3 +14,14 @@ exports['initial position'] = function (test) {
     test.equal(scanner.position(), 0);
 };
 
+exports['scan character'] = function (test) {
+    const scanner = scanners.scanner('0');
+
+    const result = scanner.scan();
+
+    test.equal(result, '0');
+
+    test.equal(scanner.position(), 1);
+    test.equal(scanner.scan(), null);
+};
+
