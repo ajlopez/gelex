3e8db84b52008867b45fe4ec2c77d45bf337d8ca
ajlopez <webmaster@ajlopez.com>
2019-02-24 17:46:15 -0300

    Get zero and one skipping spaces

--- test/gelex.js
+++ test/gelex.js
@@ -18,3 +18,21 @@ exports['get zero and one'] = function (test) {
     test.equal(lexer.next(), null);
     test.equal(lexer.position(), 2);
 };
+
+exports['get zero and one skipping spaces'] = function (test) {
+    gelex.define('zero', '0');
+    gelex.define('one', '1');
+    
+    const lexer = gelex.lexer('   0 1  ');
+    
+    var result = lexer.next();
+    
+    test.deepEqual(result, { type: 'zero', value: '0', begin: 3, end: 3 });
+
+    var result = lexer.next();
+    
+    test.deepEqual(result, { type: 'one', value: '1', begin: 5, end: 5 });
+    
+    test.equal(lexer.next(), null);
+    test.equal(lexer.position(), 8);
+};
