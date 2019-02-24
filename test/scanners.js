6cb3651e3506a32dd1150b1775822d17ff9a9a1c
ajlopez <webmaster@ajlopez.com>
2019-02-24 13:32:36 -0300

    Scanner initial position

--- test/scanners.js
+++ test/scanners.js
@@ -8,3 +8,9 @@ exports['create scanner'] = function (test) {
     test.equal(typeof scanner, 'object');
 };
 
+exports['initial position'] = function (test) {
+    const scanner = scanners.scanner();
+
+    test.equal(scanner.position(), 0);
+};
+
