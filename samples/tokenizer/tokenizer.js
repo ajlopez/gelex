2cbf0aae0ed9e927ab643af589b0990f79d92aa2
ajlopez <webmaster@ajlopez.com>
2019-03-16 20:03:15 -0300

    Updated tokenizer sample

--- samples/tokenizer/tokenizer.js
+++ samples/tokenizer/tokenizer.js
@@ -3,6 +3,9 @@ const gelex = require('../..');
 const def = gelex.definition();
 
 def.define('integer', '[0123456789][0123456789]*');
+def.define('name', '[a-zA-Z_][a-zA-Z0-9_]*');
+def.define('delimiter', [ '{', '}', ',', ';' ]);
+//def.define('operator', [ '+', '-', '*', '/', '==', '===', '**', '^', '!', '|', '||', '&', '&&' ]);
 
 const text = process.argv[2];
 
