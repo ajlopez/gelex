68074e1f800287c2155c215cdb257d68beb28d34
ajlopez <webmaster@ajlopez.com>
2019-03-17 10:00:47 -0300

    If defined new lines they are not whitespaces

--- test/array.js
+++ test/array.js
@@ -18,3 +18,22 @@ exports['get operators'] = function (test) {
 
     test.equal(lexer.next(), null);
 };
+
+exports['get new lines'] = function (test) {
+    const def = gelex.definition();
+    def.define('newline', [ '\r\n', '\r', '\n' ]);
+    
+    const lexer = def.lexer(' \r\n \n\r');
+    const expected = [ '\r\n', '\n', '\r' ];
+    
+    for (let k = 0; k < expected.length; k++) {
+        const result = lexer.next();
+
+        test.ok(result);
+        test.equal(result.type, 'newline');
+        test.equal(result.value, expected[k]);
+    }
+
+    test.equal(lexer.next(), null);
+};
+
