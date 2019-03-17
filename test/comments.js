4bebcde5cf1bffdf652d0fe17684a1fa9735a612
ajlopez <webmaster@ajlopez.com>
2019-03-17 13:03:41 -0300

    Line comments

--- test/comments.js
+++ test/comments.js
@@ -18,3 +18,20 @@ exports['skip comments'] = function (test) {
     test.equal(lexer.next(), null);
 };
 
+exports['skip line comments'] = function (test) {
+    const def = gelex.definition();
+    
+    def.define('name', '[a-z][a-z]*');
+    def.defineComment('//');
+    
+    const lexer = def.lexer('  // a line comment\r\nfoo//another line comment');
+    
+    const result = lexer.next();
+    
+    test.ok(result);
+    test.equal(result.value, 'foo');
+    test.equal(result.type, 'name');
+    
+    test.equal(lexer.next(), null);
+};
+
