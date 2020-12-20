cab9f9b8be5363bbd801901e8966900a586d4ead
ajlopez <webmaster@ajlopez.com>
2020-12-20 08:27:27 -0300

    Named sets of characters

---
+++ test/sets.js
@@ -0,0 +1,57 @@
+
+const gelex = require('..');
+
+exports['get integer'] = function (test) {
+    const def = gelex.definition();
+    def.set('digit', '0-9');
+    def.define('integer', '{digit}{digit}*');
+    
+    const lexer = def.lexer('123 42');
+    
+    const result = lexer.next();
+    
+    test.ok(result);
+    test.deepEqual(result, { type: 'integer', value: '123', begin: 0, end: 2 });
+
+    const result2 = lexer.next();
+    
+    test.ok(result2);
+    test.deepEqual(result2, { type: 'integer', value: '42', begin: 4, end: 5 });
+
+    test.equal(lexer.next(), null);
+};
+
+exports['get one digit integer'] = function (test) {
+    const def = gelex.definition();
+    def.set('digit', '0-9');
+    def.define('integer', '{digit}{digit}*');
+    
+    const lexer = def.lexer('1');
+    
+    const result = lexer.next();
+    
+    test.ok(result);
+    test.deepEqual(result, { type: 'integer', value: '1', begin: 0, end: 0 });
+};
+
+exports['get name'] = function (test) {
+    const def = gelex.definition();
+    def.set('digit', '0-9');
+    def.set('letter', 'a-zA-Z');
+    def.define('name', '[{letter}_][{letter}{digit}_]*');
+    
+    const lexer = def.lexer('foo bar42 _x _x123 foo_bar');
+    
+    const expected = [ 'foo', 'bar42', '_x', '_x123', 'foo_bar' ];
+    
+    for (let k = 0; k < expected.length; k++) {
+        const result = lexer.next();
+        
+        test.ok(result);
+        test.equal(result.type, 'name');
+        test.equal(result.value, expected[k]);
+    }
+
+    test.equal(lexer.next(), null);
+};
+
